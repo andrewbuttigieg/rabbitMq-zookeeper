273e045faa98251f036aaa261414be299fc0d04b
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2017-05-07 22:12:26 +0200

    Working demo

---
+++ RabbitMq.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMq.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 32773 };
+
+            while (true)
+            {
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to repeat.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
